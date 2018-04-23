c1d6a2b3d0f5be8d1fc1be5c7efe84c7f1dc075b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Series_DoughnutChart/Form1.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+// ...
+
+namespace Series_DoughnutChart {
+    public partial class Form1: Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a new chart.
+            ChartControl DoughnutChart = new ChartControl();
+
+            // Create a doughnut series.
+            Series series1 = new Series("Series 1", ViewType.Doughnut);
+
+            // Populate the series with points.
+            series1.Points.Add(new SeriesPoint("Russia", 17.0752));
+            series1.Points.Add(new SeriesPoint("Canada", 9.98467));
+            series1.Points.Add(new SeriesPoint("USA", 9.63142));
+            series1.Points.Add(new SeriesPoint("China", 9.59696));
+            series1.Points.Add(new SeriesPoint("Brazil", 8.511965));
+            series1.Points.Add(new SeriesPoint("Australia", 7.68685));
+            series1.Points.Add(new SeriesPoint("India", 3.28759));
+            series1.Points.Add(new SeriesPoint("Others", 81.2));
+
+            // Add the series to the chart.
+            DoughnutChart.Series.Add(series1);
+
+            // Adjust the value numeric options of the series.
+            series1.PointOptions.ValueNumericOptions.Format = NumericFormat.Percent;
+            series1.PointOptions.ValueNumericOptions.Precision = 0;
+
+            // Specify how series points are sorted.
+            series1.SeriesPointsSorting = SortingMode.Ascending;
+            series1.SeriesPointsSortingKey = SeriesPointKey.Argument;
+
+            // Specify the behavior of series labels.
+            ((DoughnutSeriesLabel)series1.Label).Position = PieSeriesLabelPosition.TwoColumns;
+            ((DoughnutSeriesLabel)series1.Label).ResolveOverlappingMode = ResolveOverlappingMode.Default;
+            ((DoughnutSeriesLabel)series1.Label).ResolveOverlappingMinIndent = 5;
+            ((PiePointOptions)series1.PointOptions).PointView = PointView.ArgumentAndValues;
+            
+            // Adjust the view-type-specific options of the series.
+            ((DoughnutSeriesView)series1.View).ExplodedPoints.Add(series1.Points[0]);
+            ((DoughnutSeriesView)series1.View).ExplodedDistancePercentage = 30;
+
+            // Access the diagram's options.
+            ((SimpleDiagram)DoughnutChart.Diagram).Dimension = 2;
+
+            // Add a title to the chart and hide the legend.
+            ChartTitle chartTitle1 = new ChartTitle();
+            chartTitle1.Text = "3D Doughnut Chart";
+            DoughnutChart.Titles.Add(chartTitle1);
+            DoughnutChart.Legend.Visible = false;
+
+            // Add the chart to the form.
+            DoughnutChart.Dock = DockStyle.Fill;
+            this.Controls.Add(DoughnutChart);
+        }
+
+    }
+}
